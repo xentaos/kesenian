b19f72aa7d4db6f44d715eafe2ca5147b9ad9d4b
Dindin Hernawan <root@dev.xentaos.org>
2017-12-05 02:28:11 +0700

    Update

---
+++ project/artwork/sticker/library/svg2png.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+# This file is part of Xenta OS Project Management.
+# 
+# svg2png.sh is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# svg2png.sh is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+figlet svg2png
+SAVEIF=$IFS
+IFS=$(echo -en "\n\b")
+for file in $(ls _xenta_os_art*svg)
+do
+  name=${file%%.svg}
+  # Silahkan Isi Tinggi dalam ukuran pixel di export berkas png
+  # Silahkan Isi Tinggi disini :
+  height="537"
+  # Silahkan Isi Lebar dalam ukuran pixel di export berkas png
+  # Silahkan Isi Lebar disini :
+  width="987"
+  echo "Export Berkas $name..."
+  inkscape $name.svg --export-png=$name.png --export-height=$height --export-width=$width
+  echo "Berhasil.."
+done
